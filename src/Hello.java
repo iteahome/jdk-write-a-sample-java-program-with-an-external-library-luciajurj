d010199fcc6314a374a8321ee49b0368f1e6781d
luciajurj <48239224+luciajurj@users.noreply.github.com>
2019-12-02 21:20:03 +0200

    Simple java program with external library

---
+++ src/Hello.java
@@ -0,0 +1,9 @@
+import org.joda.time.DateTime;
+
+public class Hello {
+    public static void main(String[] args)
+    {
+        DateTime dt = new DateTime();
+        System.out.println("The date and the time is: " +dt);
+    }
+}
